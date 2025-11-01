219b269e5d792db14059f43cf9e8318873617dd6
AniviaFlome <user.name>
2025-11-01 22:05:42 +0300

    Declerative distrobox support

--- hosts/nixos/hm-imports.nix
+++ hosts/nixos/hm-imports.nix
@@ -11,7 +11,7 @@
     # Programs
     ../../hm-modules/programs/atuin.nix
     ../../hm-modules/programs/bash.nix
-    ../../hm-modules/programs/distrobox/default.nix
+    ../../hm-modules/programs/distrobox.nix
     ../../hm-modules/programs/fastfetch/default.nix
     ../../hm-modules/programs/fish.nix
     ../../hm-modules/programs/git.nix
